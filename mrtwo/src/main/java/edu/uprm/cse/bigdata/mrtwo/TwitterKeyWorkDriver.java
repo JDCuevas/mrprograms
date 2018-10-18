0c1184ee1f4c1d9584948a859d4f20aa854d98dd
JDCuevas <jd.kronoh@gmail.com>
2018-10-18 22:30:59 +0000

    Fixing NotStopWords MR

---
+++ mrtwo/src/main/java/edu/uprm/cse/bigdata/mrtwo/TwitterKeyWorkDriver.java
@@ -0,0 +1,38 @@
+package edu.uprm.cse.bigdata.mrtwo;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * Created by julian_cuevas1 on 10/17/18.
+ */
+public class TwitterKeyWorkDriver {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: TwitterKeyWorkDriver <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = new Job();
+        job.setJarByClass(edu.uprm.cse.bigdata.mrtwo.TwitterKeyWorkDriver.class);
+        job.setJobName("Count NotStopWords");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(edu.uprm.cse.bigdata.mrtwo.TwitterKeyWordMapper.class);
+        job.setReducerClass(edu.uprm.cse.bigdata.mrtwo.TwitterKeyWorkReducer.class);
+        //job.setCombinerClass(edu.uprm.cse.bigdata.mrtwo.TwitterReduceByScreenName.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //System.println("Done!");
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
